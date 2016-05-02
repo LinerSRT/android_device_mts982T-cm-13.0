747788256bc8065e3fb312078dfa6ce0195cb8fc
SeriniTY320 <SeriniTY320@gmail.com>
2016-05-02 16:52:39 +0200

    uptade cm.mk

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := mts_982t
+PRODUCT_RELEASE_NAME := 982t
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -10,8 +10,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/mts/982t/device_982t.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mts_982t
-PRODUCT_NAME := cm_mts_982t
+PRODUCT_DEVICE := 982t
+PRODUCT_NAME := cm_982t
 PRODUCT_BRAND := MTS
 PRODUCT_MODEL := mts_982t
 PRODUCT_MANUFACTURER := MTS
